f10c8271998139f3559ff9140dcc83110a2a034a
hyeminLee7 <hmin10580@gmail.com>
2021-08-13 17:39:49 +0900

    final

--- src/main/java/shop/jarviis/api/book/BookApplication.java
+++ src/main/java/shop/jarviis/api/book/BookApplication.java
@@ -1,9 +1,17 @@
 package shop.jarviis.api.book;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootConfiguration
+@ComponentScan
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class BookApplication {
 
 	public static void main(String[] args) {
